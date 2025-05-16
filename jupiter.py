1b48e367abc88dcdf465c76bfeaab8fad46f9071
BT_4N2_31 <@bths.edu>
2025-05-16 13:19:10 -0400

    i love types

--- jupiter.py
+++ jupiter.py
@@ -1,4 +1,7 @@
 from enum import IntEnum
+from collections.abc import Callable
+from typing import Protocol
+
 from dbconnection import DBConnection
 
 
@@ -7,22 +10,48 @@ class UserTypes(IntEnum):
     TEACHER = 2
 
 
+class GetsInput(Protocol):
+    def __call__(self, has_error: bool) -> str: ...
+
+
+class ChecksSuccess[T](Protocol):
+    def __call__(self, user_input: str) -> (bool, T | None): ...
+
+
 class Jupiter:
-    def __init__(self, database):
-        self.connection = DBConnection(database)
+    def __init__(self, database) -> None:
+        self.connection: DBConnection = DBConnection(database)
         self.user_type: UserTypes | None = None
 
     def login(self):
         print("Are you a student or a teacher?")
         print("1: Student")
         print("2: Teacher")
-        user_type = input("Enter choice: ").strip()
-        user_type = int(user_type) if user_type.isdigit() else None
+        user_type: str = input("Enter choice: ").strip()
+        user_type: int | None = int(user_type) if user_type.isdigit() else None
 
         while user_type not in UserTypes:
             print("Invalid choice!")
-            user_type = input("Enter choice: ").strip()
-            user_type = int(user_type) if user_type.isdigit() else None
+            user_type: str = input("Enter choice: ").strip()
+            user_type: int | None = int(user_type) if user_type.isdigit() else None
 
-        self.user_type = UserTypes(user_type)
+        self.user_type: UserTypes = UserTypes(user_type)
 
+        if self.user_type is UserTypes.STUDENT:
+            pass
+        else:
+            pass
+
+    def _input_until_success[T](self, get_input: GetsInput,
+                                if_success: ChecksSuccess[T]) -> T:
+        user_input: str = get_input(False)
+
+        successful: bool
+        formatted_input: T | None
+        successful, formatted_input = if_success(user_input)
+
+        while not successful:
+            user_input: str = get_input(True)
+            successful, formatted_input = if_success(user_input)
+
+        return formatted_input
